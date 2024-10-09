6f3f243b1d329bdb2bb5d0161684517d03d83b46
Pablito <pablorigueto@hotmail.com>
2024-10-09 14:32:15 -0300

    keep going.

--- scripts/cam_gimbal/cam_gimbal.gd
+++ scripts/cam_gimbal/cam_gimbal.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-
 @onready var player = $"../Player-elf-archer"
 var base_position = Vector3()
 var base_rotation = Vector3()
@@ -12,3 +11,23 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = player.position
+
+	if Input.is_action_pressed('cam_rotation_left'):
+		rotation.y += 1.0 * delta
+	
+	if Input.is_action_pressed('cam_rotation_right'):
+		rotation.y -= 1.0 * delta
+		
+	if Input.is_action_pressed('cam_gimbal_up'):
+		rotation.y -= 1.0 * delta
+		rotation.x -= 0.3 * delta
+		
+	if Input.is_action_pressed('cam_gimbal_down'):
+		rotation.y += 1.0 * delta
+		rotation.x += 0.3 * delta
+		
+	if Input.is_action_just_pressed("rotate_cam"):
+		rotation_degrees.y += 90
+
+		
+		
